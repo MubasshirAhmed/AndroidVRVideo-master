5217af61a1f9556d74855b92fa780d4b21c1329f
MUBASSHIRAHMED\Mubasshir Ahmed <MubasshirAhmed26@gmail.com>
2018-08-15 15:28:33 +0600

    first commit

---
+++ AndroidVRVideo/Assets/Scripts/GyroscopeControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroscopeControl : MonoBehaviour
+{
+
+    private bool gyroEnabled;
+    private Gyroscope gyro;
+    public GameObject Player;
+    private Quaternion rot;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Input.gyro.enabled = true;
+        
+        gyroEnabled = EnableGyro();
+    }
+
+    private bool EnableGyro()
+    {
+        if(SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+
+            Player.transform.rotation = Quaternion.Euler(90f, 90f, 0);
+            rot = new Quaternion(0, 0, 1, 0);
+
+            return true;
+        }
+        return false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gyroEnabled)
+        {
+            transform.localRotation = gyro.attitude * rot;
+        }
+        //transform.Rotate(-Input.gyro.rotationRateUnbiased.x, -Input.gyro.rotationRateUnbiased.y, -Input.gyro.rotationRateUnbiased.z);
+    }
+}
---
+++ AndroidVRVideo/Assets/Scripts/VrToNormalMode.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class VrToNormalMode : MonoBehaviour
+{
+
+    public void OnVREnableButtonDown()
+    {
+        ToggleVR();
+    }
+
+   private void ToggleVR()
+    {
+        if(XRSettings.loadedDeviceName == "cardboard")
+        {
+            StartCoroutine(LoadDevice("None"));
+        }
+        else
+        {
+            StartCoroutine(LoadDevice("cardboard"));
+        }
+    }
+
+    IEnumerator LoadDevice(string newDevice)
+    {
+        XRSettings.LoadDeviceByName(newDevice);
+        yield return null;
+        XRSettings.enabled = true;
+    }
+}
